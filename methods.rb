8491c551a68f9e99d8ecb39f0de9162db399b644
Elizabeth Rios-Mujica <elizabeth.rmujica@gmail.com>
2022-06-10 10:18:45 -0400

    commit tree 6e379a7091052a9ac1cf7d348018f7af3e200670

--- methods.rb
+++ methods.rb
@@ -1,1 +1,38 @@
 # Your code here!
+
+def greet_programmer()
+ puts "Hello, programmer!"
+end
+
+
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+greet
+
+
+def greet_with_default(name = "Jimmy")
+    puts "Hello, #{name}!"
+    puts "Hello, programmer!"
+end
+greet_with_default
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+
+def halve(num1)
+    return num1 / 2
+    return nil
+end
+
+def halve(num1)
+    if num1.is_a?(Integer)
+     num1 / 2
+    else
+        nil
+    end
+end
